0805c160dbf80ba57743277f73e51ee6d323e62d
Rubul-Tanti <rubultanti404@gmail.com>
2025-09-01 22:20:46 +0530

    small fixes

--- db/config.js
+++ db/config.js
@@ -4,6 +4,7 @@ const { ApiError } = require("../middleware/Error");
 
 const connectToDb = async () => {
   try {
+    console.log(Env.mongourl)
     mongoose.set("strictQuery", true); // optional, but recommended
     await mongoose.connect(Env.mongourl, {
       useNewUrlParser: true,
